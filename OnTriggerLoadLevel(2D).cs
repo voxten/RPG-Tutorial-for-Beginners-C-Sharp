dbcb6b7b9dc9debed116fc70ef810805aa950cc7
voxten <52979294+voxten@users.noreply.github.com>
2019-07-16 23:13:53 +0200

    Update and rename OnTriggerLoadLevelV2(2D) to OnTriggerLoadLevel(2D).cs

--- OnTriggerLoadLevel(2D).cs
+++ OnTriggerLoadLevel(2D).cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class OnTriggerLoadLevelV2 : MonoBehaviour
+public class OnTriggerLoadLevel(2D) : MonoBehaviour
 {
 
     public GameObject enterText;
