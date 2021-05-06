62ab63f0081d5687f277957870882ee6c9098482
voxten <52979294+voxten@users.noreply.github.com>
2021-05-06 23:56:29 +0200

    Update NPCWalkAI.cs

--- NPCWalkAI.cs
+++ NPCWalkAI.cs
@@ -4,33 +4,30 @@ using UnityEngine;
 
 public class NPCWalkAI : MonoBehaviour
 {
-    public int Xpos;
-    public int Zpos;
-    public GameObject NPCDest;
-
-
+	public int Xpos;
+	public int Zpos;
+	public GameObject NPCDest; //NPC Destination
+	
 	void Start ()
-	{
-        Xpos = Random.Range(488, 510);
-        Zpos = Random.Range(461, 480);
-        NPCDest.transform.position = new Vector3(Xpos, 0, Zpos);
-        StartCoroutine(RunRandomWalk());
+    	{
+    		Xpos = Random.Range(488, 510);
+       		Zpos = Random.Range(461, 480);
+        	NPCDest.transform.position = new Vector3(Xpos, 0, Zpos);
+        	StartCoroutine(RunRandomWalk());
 	}
 	
-
 	void Update ()
 	{
-        transform.LookAt(NPCDest.transform);
-        transform.position = Vector3.MoveTowards(transform.position, NPCDest.transform.position, 0.01f * Time.timeScale);
+        	transform.LookAt(NPCDest.transform);
+        	transform.position = Vector3.MoveTowards(transform.position, NPCDest.transform.position, 0.01f * Time.timeScale);
 	}
-
-    IEnumerator RunRandomWalk ()
-    {
-        yield return new WaitForSeconds(3);
-        Xpos = Random.Range(488, 510);
-        Zpos = Random.Range(461, 480);
-        NPCDest.transform.position = new Vector3(Xpos, 0, Zpos);
-        StartCoroutine(RunRandomWalk());
-    }
+	IEnumerator RunRandomWalk ()
+    	{
+        	yield return new WaitForSeconds(3);
+        	Xpos = Random.Range(488, 510);
+       		Zpos = Random.Range(461, 480);
+        	NPCDest.transform.position = new Vector3(Xpos, 0, Zpos);
+        	StartCoroutine(RunRandomWalk());
+    	}
 }
 
