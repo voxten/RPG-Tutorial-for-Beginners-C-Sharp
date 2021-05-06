8e095447d75f4818819420abbde232e980f2a232
voxten <52979294+voxten@users.noreply.github.com>
2021-05-06 12:05:01 +0200

    Update GoblinAI.cs

--- GoblinAI.cs
+++ GoblinAI.cs
@@ -3,58 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GoblinAI : MonoBehaviour
- {
-	    public GameObject ThePlayer;
+{
+	public GameObject ThePlayer;
     	public float TargetDistance;
     	public float AllowedRange = 20;
     	public GameObject TheEnemy;
     	public float EnemySpeed;
     	public int AttackTrigger;
     	public RaycastHit Shot;
-		  public int DealingDamage;
+	public int DealingDamage;
 
 	void Update ()
 	{
-		    transform.LookAt (ThePlayer.transform);
-        if(Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), out Shot))
-        {
-            TargetDistance = Shot.distance;
-            if (TargetDistance <= AllowedRange)
-            {
-                EnemySpeed = 0.02f;
-                if (AttackTrigger == 0)
-                {
-                    TheEnemy.GetComponent<Animation>().Play("run");
-                    transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, EnemySpeed * Time.timeScale);
-                }
-            }
-            else
-            {
-                EnemySpeed = 0;
-                TheEnemy.GetComponent<Animation>().Play("combat_idle");
-            }
-        }
-        if (AttackTrigger == 1)
-        {
+		transform.LookAt (ThePlayer.transform);
+        	if(Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), out Shot))
+        	{
+            		TargetDistance = Shot.distance;
+            		if (TargetDistance <= AllowedRange)
+            	{
+                	EnemySpeed = 0.02f;
+                	if (AttackTrigger == 0)
+                	{
+                    		TheEnemy.GetComponent<Animation>().Play("run");
+                    		transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, EnemySpeed * Time.timeScale);
+                	}
+			}
+			else
+       			{
+                		EnemySpeed = 0;
+                		TheEnemy.GetComponent<Animation>().Play("combat_idle");
+        		}
+        	}
+        	if (AttackTrigger == 1)
+        	{
 			if (DealingDamage == 0)
 			{
-				  EnemySpeed = 0;
-          TheEnemy.GetComponent<Animation>().Play("attack3");
-				  StartCoroutine(TakingDamage());
+				EnemySpeed = 0;
+          			TheEnemy.GetComponent<Animation>().Play("attack3");
+				StartCoroutine(TakingDamage());
 			}
            
-        }
+       		}
 	}
 
-    void OnTriggerEnter()
-    {
-        AttackTrigger = 1;
-    }
+    	void OnTriggerEnter()
+    	{
+        	AttackTrigger = 1;
+    	}
 
-    void OnTriggerExit()
-    {
-        AttackTrigger = 0;
-    }
+    	void OnTriggerExit()
+    	{
+        	AttackTrigger = 0;
+    	}
 	
 	IEnumerator TakingDamage()
 	{
