9ec0f500d0102f6c3861aac08fa9a38941aac40f
voxten <52979294+voxten@users.noreply.github.com>
2021-05-06 12:09:38 +0200

    Update PauseMenu.cs

--- PauseMenu.cs
+++ PauseMenu.cs
@@ -2,27 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class PauseMenu : MonoBehaviour {
-    
-     public static bool GameIsPaused = false;
-    
-	   public GameObject pauseMenuUI;
+public class PauseMenu : MonoBehaviour 
+{
 	
-	
-	// Update is called once per frame
-    void Update(){
-        if(Input.GetKeyDown(KeyCode.Escape))
+    	public static bool GameIsPaused = false;
+    	public GameObject pauseMenuUI;
+    
+    	void Update()
+    	{
+    		if(Input.GetKeyDown(KeyCode.Escape))
 		{
-		  if (GameIsPaused) 
-		{
-			  Resume();
-		  } else
-		  { 
-			  Pause();
-		  }
-
+			if (GameIsPaused) 
+			{
+				Resume();
+			} 
+			else
+			{ 
+				Pause();
+			}
 		}
-  }
+  	}
 	
 	public void Resume ()
 	{
@@ -31,7 +30,7 @@ public class PauseMenu : MonoBehaviour {
 		GameIsPaused = false;
 	}
 
-  void Pause ()
+  	void Pause ()
 	{
 		pauseMenuUI.SetActive(true);
 		Time.timeScale = 0f;
@@ -41,13 +40,13 @@ public class PauseMenu : MonoBehaviour {
 	public void LoadMenu()
 	{
 		Time.timeScale = 1f;
-	   SceneManager.LoadScene("Menu");
+	   	SceneManager.LoadScene("Menu");
 	}
 	
 	public void QuitGame()
 	{
-	   Debug.Log("Quitting game...");
-	   Application.Quit();
+	   	Debug.Log("Quitting game...");
+	   	Application.Quit();
 	}
 	
 }
