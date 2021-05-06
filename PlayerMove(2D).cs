223bb93777aa6a2a4ffa212d3afa209bb615921d
voxten <52979294+voxten@users.noreply.github.com>
2021-05-06 12:14:32 +0200

    Update PlayerMove(2D).cs

--- PlayerMove(2D).cs
+++ PlayerMove(2D).cs
@@ -4,23 +4,16 @@ using UnityEngine;
 
 public class PlayerMove(2D) : MonoBehaviour
 {
-    
-	public float moveSpeed;
-	
+    	public float moveSpeed;
 	private Animator anim;
 	
-// Start is called before the first frame update
-    void Start()
-    {
-        anim = GetComponent<Animator> ();
-    }
+    	void Start()
+   	{
+        	anim = GetComponent<Animator> ();
+    	}
 
-    // Update is called once per frame
-    void Update()
-    {
-       
-		
-		
+    	void Update()
+    	{
 		if(Input.GetKey(KeyCode.D))
 		{
 		       GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
@@ -30,9 +23,7 @@ public class PlayerMove(2D) : MonoBehaviour
 		{
 		       GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		}
-		   anim.SetFloat("Speed",Mathf.Abs(GetComponent<Rigidbody2D> ().velocity.x));
 		
-		
-		
-    }
+		anim.SetFloat("Speed",Mathf.Abs(GetComponent<Rigidbody2D> ().velocity.x));
+    	}
 }
