1d8bc48b20c1d19c8cc7de683815dbc2bf60df7e
voxten <52979294+voxten@users.noreply.github.com>
2021-05-06 23:50:10 +0200

    Update SettingsMenu.cs

--- SettingsMenu.cs
+++ SettingsMenu.cs
@@ -8,29 +8,22 @@ public class SettingsMenu : MonoBehaviour
 {
     
 	public AudioMixer audioMixer;
-	
 	public Dropdown resolutionDropdown;
-	
 	Resolution[] resolutions;
 	
 	void Start ()
 	{
-	    resolutions = Screen.resolutions;
-		
+		resolutions = Screen.resolutions;
 		resolutionDropdown.ClearOptions();
-		
 		List<string> options = new List<string>();
-		
 		int currentResolutionIndex = 0;
 		for (int i = 0; i < resolutions.Length; i++)
 		{
 			string option = resolutions[i].width + " x " + resolutions[i].height;
 			options.Add(option);
-			
-			if (resolutions[i].width == Screen.width && 
-			    resolutions[i].height == Screen.height)
+			if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
 			{
-			    currentResolutionIndex = i;
+				currentResolutionIndex = i;
 			}
 		}
 		
@@ -38,6 +31,7 @@ public class SettingsMenu : MonoBehaviour
 		resolutionDropdown.value = currentResolutionIndex;
 		resolutionDropdown.RefreshShownValue();
 	}
+	
 	public void SetResolution (int resolutionIndex)
 	{
 		 Resolution resolution = resolutions[resolutionIndex];
@@ -46,13 +40,12 @@ public class SettingsMenu : MonoBehaviour
 	
 	public void SetVolume (float volume)
 	{
-	    audioMixer.SetFloat("volume", volume);
+		audioMixer.SetFloat("volume", volume);
 	}
 
 	public void SetQuality (int qualityIndex)
 	{
 		QualitySettings.SetQualityLevel(qualityIndex);
-		
 	}
 	
 	public void SetFullscreen (bool isFullscreen)
