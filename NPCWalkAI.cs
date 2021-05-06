e0fffe16d06415e455c49b5781b2f9ffa1da09f7
voxten <52979294+voxten@users.noreply.github.com>
2021-05-06 23:54:29 +0200

    Create NPCWalkAI.cs

---
+++ NPCWalkAI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCWalkAI : MonoBehaviour
+{
+    public int Xpos;
+    public int Zpos;
+    public GameObject NPCDest;
+
+
+	void Start ()
+	{
+        Xpos = Random.Range(488, 510);
+        Zpos = Random.Range(461, 480);
+        NPCDest.transform.position = new Vector3(Xpos, 0, Zpos);
+        StartCoroutine(RunRandomWalk());
+	}
+	
+
+	void Update ()
+	{
+        transform.LookAt(NPCDest.transform);
+        transform.position = Vector3.MoveTowards(transform.position, NPCDest.transform.position, 0.01f * Time.timeScale);
+	}
+
+    IEnumerator RunRandomWalk ()
+    {
+        yield return new WaitForSeconds(3);
+        Xpos = Random.Range(488, 510);
+        Zpos = Random.Range(461, 480);
+        NPCDest.transform.position = new Vector3(Xpos, 0, Zpos);
+        StartCoroutine(RunRandomWalk());
+    }
+}
+
