9167e2d50cf8c2775f114be3cd563784886f2473
voxten <52979294+voxten@users.noreply.github.com>
2021-05-06 12:01:55 +0200

    Update LevelChanger.cs

--- LevelChanger.cs
+++ LevelChanger.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelChanger : MonoBehaviour {
+public class LevelChanger : MonoBehaviour 
+{
    
  
     public Animator animator;
@@ -9,23 +10,24 @@ public class LevelChanger : MonoBehaviour {
     private int levelToLoad;
 
     // Update is called once per frame
-    void Update(){
-	  if (Input.GetMouseButtonDown(0))
-      {
-		  FadeToLevel(1);       
-      }
-  }
+    void Update()
+    {	
+	if (Input.GetMouseButtonDown(0))
+        {
+		FadeToLevel(1);       
+        }
+    }
 	
-	 public void FadeToLevel (int levelIndex)
-	 {
-		 levelToLoad = levelIndex;
-		 animator.SetTrigger("FadeOut");
-	 }
+	public void FadeToLevel (int levelIndex)
+	{
+		levelToLoad = levelIndex;
+		animator.SetTrigger("FadeOut"); 
+	}
 	
 	
 	public void OnFadeComplete ()
 	{
-		 SceneManager.LoadScene(levelToLoad);
+		SceneManager.LoadScene(levelToLoad);
 	}
 	
 }
