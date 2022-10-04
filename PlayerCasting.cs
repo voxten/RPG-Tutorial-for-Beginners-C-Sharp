293f8773407bef372918b5dcf391e21d62ecd2a8
voxten <voxterman@gmail.com>
2022-10-04 12:40:28 +0200

    Modified PlayerCasting.cs
    Changed script variables

--- PlayerCasting.cs
+++ PlayerCasting.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class PlayerCasting : MonoBehaviour 
 {
-	public static float DistanceFromTarget;
-	public float ToTarget;
+    public static float distanceFromTarget;
+    private float _toTarget;
 
-	void Update () 
-	{
-		RaycastHit Hit;
-		if (Physics.Raycast (transform.position, transform.TransformDirection (Vector3.forward), out Hit)) 
-		{
-			ToTarget = Hit.distance;
-			DistanceFromTarget = ToTarget;
-		}
-	}
+    private void FixedUpdate() 
+    {
+        RaycastHit Hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit)) 
+        {
+            _toTarget = Hit.distance;
+            distanceFromTarget = _toTarget;
+        }
+    }
 }
