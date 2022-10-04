b4a2916842b4ec0e0f0af03837fdf9d057ff069f
voxten <voxterman@gmail.com>
2022-10-04 12:48:32 +0200

    Modified MainMenu.cs
    Modified visually whole script

--- MainMenu.cs
+++ MainMenu.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class MainMenu : MonoBehaviour 
 {
-
-
    public void PlayGame()
    {
-         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-         //or SceneManager.LoadScene("NameOfScene");
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //or SceneManager.LoadScene("NameOfScene");
    }
 
    public void QuitGame ()
    {
-         Debug.Log("QUIT!");
-         Application.Quit();
+      Debug.Log("QUIT!");
+      Application.Quit();
    }
 }
