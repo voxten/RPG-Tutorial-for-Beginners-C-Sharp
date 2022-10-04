e4c61685d8a80e7e5f957b0211cb78067602bbfc
voxten <voxterman@gmail.com>
2022-10-04 12:37:35 +0200

    Modified PlayerMove(2D) With Scale
    Changed visually whole script and Added SerializeFields

--- PlayerMove(2D) With Scale.cs
+++ PlayerMove(2D) With Scale.cs
@@ -4,34 +4,34 @@ using UnityEngine;
 
 public class PlayerMove(2D) With Scale : MonoBehaviour
 {
-    
-	public float moveSpeed;
-	private Animator anim;
-	void Start()
-    	{
-        	anim = GetComponent<Animator> ();
-    	}
+    [SerializeField] private float float moveSpeed;
+    [SerializeField] private Animator anim;
 
-	void Update()
-    	{
-		if(Input.GetKey(KeyCode.D))
-		{
-		       GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-		}
-		if(Input.GetKey(KeyCode.A))
-		{
-		       GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-		}
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
 		
-		anim.SetFloat("Speed",Mathf.Abs(GetComponent<Rigidbody2D> ().velocity.x));
+        anim.SetFloat("Speed",Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
 		
-		if (GetComponent<Rigidbody2D> ().velocity.x > 0)
-		{
-			transform.localScale = new Vector3 (1f, 1f, 1f);
-		}
-		else if (GetComponent<Rigidbody2D> ().velocity.x < 0)
-		{
-			transform.localScale = new Vector3 (-1f, 1f, 1f);
-		}
-	}
+        if(GetComponent<Rigidbody2D>().velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if(GetComponent<Rigidbody2D>().velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+    }
 }
