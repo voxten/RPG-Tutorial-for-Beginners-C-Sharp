d2b7523ce8ddcf618285cdaf3569ab08bb3c727c
voxten <voxterman@gmail.com>
2022-10-04 12:42:59 +0200

    Modified PauseMenu.cs

--- PauseMenu.cs
+++ PauseMenu.cs
@@ -4,51 +4,47 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour 
 {
-	
-    	public static bool GameIsPaused = false;
-    	public GameObject pauseMenuUI;
+    public static bool isPaused = false;
+    [SerializeField] private GameObject pauseMenuUI;
     
-    	void Update()
-    	{
-    		if(Input.GetKeyDown(KeyCode.Escape))
-		{
-			if (GameIsPaused) 
-			{
-				Resume();
-			} 
-			else
-			{ 
-				Pause();
-			}
-		}
-  	}
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused) 
+            {
+                Resume();
+            } 
+            else
+            { 
+                Pause();
+            }
+        }
+    }
 	
-	public void Resume ()
-	{
-		pauseMenuUI.SetActive(false);
-		Time.timeScale = 1f;
-		GameIsPaused = false;
-	}
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
 
-  	void Pause ()
-	{
-		pauseMenuUI.SetActive(true);
-		Time.timeScale = 0f;
-		GameIsPaused = true;
-	}
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
 	
-	public void LoadMenu()
-	{
-		Time.timeScale = 1f;
-	   	SceneManager.LoadScene("Menu");
-	}
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
 	
-	public void QuitGame()
-	{
-	   	Debug.Log("Quitting game...");
-	   	Application.Quit();
-	}
-	
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
 }
-
-
