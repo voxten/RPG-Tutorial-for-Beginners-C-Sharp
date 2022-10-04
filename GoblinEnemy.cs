a3e084a39c60efab85a8903d75204a3462f98755
voxten <voxterman@gmail.com>
2022-10-04 13:06:07 +0200

    Modified GoblinEnemy.cs
    Changed variables and modified visually whole script, added SerializeFields

--- GoblinEnemy.cs
+++ GoblinEnemy.cs
@@ -1,53 +1,97 @@
 using System.Collections;
-using System.Collections.Generic;
+using Experience;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GoblinEnemy : MonoBehaviour 
+namespace Enemy.Goblin
 {
-	public int EnemyHealth = 10;
-	public static int Health;
-	public GameObject TheGoblin;
-	public int GoblinStatus;
-	public int BaseXP = 60;
-	public int CalculatedExp;
-	public GoblinAI GoblinAIScript;
-	public GoblinShowHealth GoblinShowHealthScript;
-	public static int GlobalGoblin;
-	
-	void Start()
+	public class GoblinEnemy : MonoBehaviour 
 	{
-	  	GoblinAIScript = GetComponent<GoblinAI>();
-		GoblinShowHealthScript = GetComponent<GoblinShowHealth>();
-	}
-	
-	void DeductPoints (int DamageAmount) 
-	{
-	  	EnemyHealth -= DamageAmount;
-	}
+		[Header("Enemy")]
+		[SerializeField] private GameObject theEnemy;
 
-	void Update () 
-	{
-		Health = EnemyHealth;
-		GlobalGoblin = GoblinStatus;
-		if (EnemyHealth <= 0) 
+		[Header("Enemy Variables")]
+		[SerializeField] private int _enemyHealth;
+		[SerializeField] private int _baseXp;
+		private int _calculatedExp;
+		private bool _isDead;
+	
+		[Header("Items")]
+		[SerializeField] private GameObject bag;
+		[SerializeField] private GameObject itemStash;
+		
+		[Header("Scripts")]
+		[SerializeField] private GoblinAI goblinAIScript;
+		[SerializeField] private GoblinShowHealth goblinShowHealthScript;
+
+		[Header("Exp")]
+		[SerializeField] private GameObject xpContent;
+		[SerializeField] private GameObject xpBlock;
+		private GameObject _xpShower;
+		private Transform _xpNumber;
+
+		[Header("Canvas")]
+		[SerializeField] private GameObject Canvas;
+
+		private void Start()
 		{
-			if (GoblinStatus == 0) 
+			//enemyHealth = MonstersData.Instance.GoblinHealth;
+			//_baseXp = MonstersData.Instance.GoblinExp;	
+			goblinAIScript = GetComponent<GoblinAI>();
+			goblinShowHealthScript = GetComponent<GoblinShowHealth>();
+		}
+
+		private void DeductPoints(int _damageAmount) 
+		{
+			_enemyHealth -= _damageAmount;
+		}
+
+		private void Update() 
+		{
+			if (_enemyHealth <= 0) 
 			{
-				StartCoroutine (DeathGoblin ());
+				if (_isDead == false) 
+				{
+					StartCoroutine(DeathGoblin());
+				}
 			}
 		}
-	}
-     
-     	IEnumerator DeathGoblin () 
-	{
-		GoblinAIScript.enabled = false;		
-		GoblinStatus = 6;
-		CalculatedExp = BaseXP * GlobalLevel.CurrentLevel;
-		GlobalExp.CurrentExp += CalculatedExp;
-		yield return new WaitForSeconds (0.5f);
-		GetComponent<BoxCollider>().enabled = false;
-		GetComponent<CapsuleCollider>().enabled = false;
-		GoblinShowHealthScript.enabled = false;
-		TheGoblin.GetComponent<Animation>().Play("death");
+
+		private IEnumerator DeathGoblin() 
+		{
+			goblinAIScript.enabled = false;	
+			goblinShowHealthScript.enabled = false;	
+			_isDead = true;
+
+			_calculatedExp = _baseXp * GlobalExp.CurrentLevel;
+			GlobalExp.CurrentExp += _calculatedExp;
+
+			itemStash.transform.SetParent(Canvas.transform);
+			itemStash.SetActive(false);
+
+			_xpShower = Instantiate(xpBlock, xpContent.transform);
+			_xpShower.SetActive(true);
+			_xpShower.transform.SetParent(xpContent.transform);
+
+			_xpNumber = _xpShower.transform.Find("xpCount/xp");
+			_xpNumber.transform.GetComponent<Text>().text = "+" + _baseXp;
+
+			yield return new WaitForSeconds (0.5f);
+
+			GetComponent<BoxCollider>().enabled = false;
+			GetComponent<CapsuleCollider>().enabled = false;
+			GetComponent<Rigidbody>().isKinematic = true;
+
+			theEnemy.GetComponent<Animation>().Play("death");
+
+			yield return new WaitForSeconds (0.5f);
+
+			Bag.SetActive(true);
+
+			Destroy(_xpShower, 5);
+			Destroy(theEnemy, 5);
+			Destroy(itemStash, 900);
+			Destroy(gameObject, 900);
+		}
 	}
 }
