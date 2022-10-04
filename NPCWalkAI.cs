9b7ee40166493e7c529e3188eb8ab5579cb42893
voxten <voxterman@gmail.com>
2022-10-04 12:47:14 +0200

    Modified NPCWalkAI.cs
    Changed variables and modified visually whole script, added SerializeFields

--- NPCWalkAI.cs
+++ NPCWalkAI.cs
@@ -4,30 +4,43 @@ using UnityEngine;
 
 public class NPCWalkAI : MonoBehaviour
 {
-	public int Xpos;
-	public int Zpos;
-	public GameObject NPCDest; //NPC Destination
+	[Header("NPC")]
+	[SerializeField] private int walkTimeChange = 3;
+	[SerializeField] private GameObject npcDest; 
 	
-	void Start ()
-	{
-    		Xpos = Random.Range(488, 510);
-       		Zpos = Random.Range(461, 480);
-        	NPCDest.transform.position = new Vector3(Xpos, 0, Zpos);
-        	StartCoroutine(RunRandomWalk());
-	}
-	
-	void Update ()
-	{
-        	transform.LookAt(NPCDest.transform);
-        	transform.position = Vector3.MoveTowards(transform.position, NPCDest.transform.position, 0.01f * Time.timeScale);
-	}
-	IEnumerator RunRandomWalk ()
-    	{
-        	yield return new WaitForSeconds(3);
-        	Xpos = Random.Range(488, 510);
-       		Zpos = Random.Range(461, 480);
-        	NPCDest.transform.position = new Vector3(Xpos, 0, Zpos);
-        	StartCoroutine(RunRandomWalk());
-    	}
-}
+	[Header("X Position")]
+	[SerializeField] private int xMinPos;
+	[SerializeField] private int xMaxPos;
+	private int _xPos;
 
+	[Header("Z Position")]
+	[SerializeField] private int zMinPos;
+	[SerializeField] private int zMaxPos;
+	private int _zPos;
+	
+	private void Start()
+	{
+		RangeInitialization()
+		StartCoroutine(RunRandomWalk());
+	}
+	
+	private void Update()
+	{
+		transform.LookAt(npcDest.transform);
+		transform.position = Vector3.MoveTowards(transform.position, npcDest.transform.position, 0.01f * Time.timeScale);
+	}
+
+	private IEnumerator RunRandomWalk()
+	{
+		yield return new WaitForSeconds(walkTimeChange);
+		RangeInitialization()
+		StartCoroutine(RunRandomWalk());
+	}
+
+	private void RangeInitialization()
+	{
+		_xPos = Random.Range(xMinPos, xMaxPos);
+		_zPos = Random.Range(zMinPos, zMaxPos);
+		npcDest.transform.position = new Vector3(_xPos, 0, _zPos);
+	}
+}
