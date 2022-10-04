32b04719d8116e364b4e5a7fe86c82b5f38bd2e9
voxten <voxterman@gmail.com>
2022-10-04 12:54:33 +0200

    Modified GoblinShowHealth.cs
    Changed variables and modified visually whole script, added SerializeFields

--- GoblinShowHealth.cs
+++ GoblinShowHealth.cs
@@ -1,37 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
+using Player;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GoblinShowHealth : MonoBehaviour
+namespace Enemy.Goblin
 {
-	public float TheDistance;
-	public GameObject EnemyText;
-	public GameObject healthGoblin;
-	public GameObject healthBar;
-	private int healthValue;
-
-	void Update () 
+	public class GoblinShowHealth : MonoBehaviour
 	{
-		healthValue = GoblinEnemy.Health;
-		TheDistance = PlayerCasting.DistanceFromTarget;
-		healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(healthValue, 20);
-	}
-
-	void OnMouseOver () 
-	{
-		if (TheDistance <= 10) 
-		{
-			EnemyText.GetComponent<Text> ().text = "Goblin";
-			EnemyText.SetActive (true);
-			healthGoblin.SetActive (true);
-		}		
-	}
-
-	void OnMouseExit() 
-	{
-		EnemyText.SetActive (false);
-		healthGoblin.SetActive (false);
-	}
+		[Header("Distance")]
+		[SerializeField] private float _theDistance;
 	
+		[Header("Canvas Objects")]
+		[SerializeField] private GameObject monsterText;
+		[SerializeField] private GameObject healthEnemy;
+		[SerializeField] private GameObject healthBar;
+
+		[Header("Enemy Health")]
+		private int _healthValue;
+
+		[Header("Scripts")]
+		public GoblinEnemy goblinEnemyScript;
+
+		private void Start() 
+		{
+			goblinEnemyScript = GetComponent<GoblinEnemy>();		
+		}
+
+		private void Update() 
+		{
+			_healthValue = goblinEnemyScript.enemyHealth;
+			_theDistance = PlayerCasting.distanceFromTarget;
+
+			if (PlayerCasting.isUp)
+			{
+				monsterText.SetActive(false);
+				healthEnemy.SetActive(false);
+			}
+		}
+
+		private void OnMouseOver() 
+		{
+			if (HealthMonitor.Dead == false)
+			{
+				if (goblinEnemyScript.isDead == false)
+				{
+					if (PlayerCasting.isUp == false)
+					{
+						if (_theDistance <= 10) 
+						{
+							monsterText.GetComponent<Text> ().text = "Goblin";
+							healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2((_healthValue * 45), 20);
+							monsterText.SetActive(true);
+							healthEnemy.SetActive(true);
+						}
+						else 
+						{
+							monsterText.SetActive(false);
+							healthEnemy.SetActive(false);
+						}	
+					}
+
+				}
+				else 
+				{
+					healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 20);
+				}
+			}
+		}
+
+		private void OnMouseExit() 
+		{
+			monsterText.SetActive(false);
+			healthEnemy.SetActive(false);
+		}
+	}
 }
